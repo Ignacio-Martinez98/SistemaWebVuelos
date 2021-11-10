649c8128bcc755468d8eb336c8fe48577cce6c7c
Ignacio Martinez <martinezigna98@gmail.com>
2021-11-10 14:32:08 -0300

    Agregar archivos de proyecto.

---
+++ SistemaWebVuelos/Data/VueloDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SistemaWebVuelos.Models;
+
+namespace SistemaWebVuelos.Data
+{
+    public class VueloDbContext:DbContext
+    {
+       public VueloDbContext() : base("KeyDb") { }
+       public DbSet<Vuelo> Vuelos { get; set; }
+    }
+}
---
+++ SistemaWebVuelos/Models/Vuelo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaWebVuelos.Models
+{
+    [Table("Vuelo")]
+    public class Vuelo
+    {
+        public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public string Fecha { get; set; }
+
+        [Required]
+        [Column(TypeName="varchar")]
+        [StringLength(50)]
+        public string Destino { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Origen { get; set; }
+
+        [Range(100,1000)]
+        public int Matricula { get; set; }
+    }
+}
